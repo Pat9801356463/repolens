8b64ffd148cf1bc849e4747615cafd7f88cc254c
Pat9801356463 <pratyush9801356463@gmail.com>
2026-09-06 20:44:18 +0530

    Delete demo_repo directory

--- demo_repo/services/notifier.py
+++
@@ -1,6 +0,0 @@
-def send_receipt(customer_id, amount):
-    print(f"Sending receipt to {customer_id} for {amount}")
-
-
-def send_alert(message):
-    print(f"ALERT: {message}")
--- demo_repo/tests/test_payment.py
+++
@@ -1,5 +0,0 @@
-from services.payment import process_payment
-
-
-def test_process_payment():
-    process_payment(None)
